c10dc4363ea948c3c84a16530cb1078fbb8329ea
Mad Triathlete <h_bicakci@msn.com>
2025-08-30 23:46:09 -0400

    new create user test added

---
+++ tests/bookstore.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+const baseUrl = 'https://bookstore.toolsqa.com';
+const createUserEndpoint = '/Account/v1/User';
+const url = `${baseUrl}${createUserEndpoint}`;
+
+const userName = 'adminUser';
+const password = 'adminUser@123';
+
+test('Create a new user', async ({ request }) => {
+  const response = await request.post(url, {
+    data: {
+      userName: userName,
+      password: password
+    },
+  });
+
+  expect(response.status()).toBe(201);
+
+  const responseBody = await response.json();
+
+  expect(responseBody).toHaveProperty('userID');
+  expect(responseBody.userID).not.toBeNull();
+  expect(responseBody.username).toBe(userName);
+
+  expect(responseBody).not.toHaveProperty('code');
+  expect(responseBody).not.toHaveProperty('message');
+
+  console.log('✅ User created successfully:', responseBody);
+});
